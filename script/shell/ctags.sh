9ce426f70c9fcd8a6204060bf7e1284107241067
dillyant <dilly.ant@gmail.com>
2015-05-14 18:00:28 +0800

    add inotify rbtree shell

---
+++ script/shell/ctags.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ctags -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q  -f ~/.vim/systags /usr/include/* /usr/include/sys/* /usr/include/bits/*  /usr/include/netinet/* /usr/include/arpa/* /usr/include/mysql/*
+
+
