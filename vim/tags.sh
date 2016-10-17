bcbc25bb0beec3502241b7c569a27c005d015802
dillyant <dillyant@gmail.com>
2016-10-17 18:03:28 +0800

    添加tags.sh 修改tags.sh

--- vim/tags.sh
+++ vim/tags.sh
@@ -4,4 +4,5 @@ find `pwd` -name "*.[ch]" -o -name "*.cpp" > cscope.files
 
 cscope -bkq -i cscope.files
 
-ctags -R
+ctags -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes  --languages=c,c++ --links=yes --c++-kinds=+p --fields=+iaS --extra=+q -R
+
