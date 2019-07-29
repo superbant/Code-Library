18b3c7ae72bb688c23f9703fa36415e89024f184
dillyant <dilly.ant@gmail.com>
2019-07-29 14:34:11 +0800

    Update tags.sh

--- vim/tags.sh
+++ vim/tags.sh
@@ -4,5 +4,5 @@ find `pwd` -name "*.[ch]" -o -name "*.cpp" > cscope.files
 
 cscope -bkq -i cscope.files
 
-ctags -R -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --languages=c,c++ --c++-kinds=+p --fields=+iaS --extra=+q 
+ctags -R -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --languages=c,c++ --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q 
 
